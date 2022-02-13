0b0b1d2bda4f0df8e9e7c58f45190375cefdc8cf
Idan Gomberg <crackenyogs10@gmail.com>
2022-02-13 15:08:54 -0500

    Added basic BootStrap CSS and commenting to the index

--- addToLearn.php
+++ addToLearn.php
@@ -6,13 +6,16 @@
         <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
     </head>
     <body>
+        <main class="container">
         <h1>Add something to learn</h1>
         <form method="post" action="saveToLearn.php">
-            <fieldset>
+            <fieldset class="input-group m-1">        
                 <label for="toLearn" class="control-label"></label>
-                <input name="toLearn" id="toLearn">
+                <input name="toLearn" id="toLearn" type="text" class="form-control col-2" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
             </fieldset>
+            
             <button>Save</button>
         </form>
     </body>
+</main>
 </html>
