9cb3cab396880dd7f265a7c0b6359a1d0ac91e9c
Idan Gomberg <crackenyogs10@gmail.com>
2022-02-13 22:21:36 -0500

    Added navbar to every page

--- addProject.php
+++ addProject.php
@@ -6,6 +6,34 @@
         <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
     </head>
     <body>
+        <!-- BootStrap navBar edited to fit my layout
+        https://getbootstrap.com/docs/4.0/components/navbar/-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-secondary my-8 my-lg-0">
+        <section class="sectionOne">
+            <a class="navbar-brand" href="#">Learning/Resource Tracker</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="addProject.php">Add Project</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="addToLearn.php">Add a Hobby</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="addResource.php">Add a Resource</a>
+                </li>
+            </form>
+        </div>
+        </section>
+        </nav>
     <main class="container">
         <h1>Add a project</h1>
         <!-- call the saveProject.php and parse the input data -->
